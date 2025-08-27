2561da575a0789f0fbfa0187fabe4f00d1c8db24
Julian Maynes <thelastpocket@gmail.com>
2025-08-27 02:20:58 -0700

    Make chord recognition. Need to check mixo

---
+++ build/lib/pychord/__init__.py
@@ -0,0 +1,45 @@
+from pychord.const import *
+from pychord.chord import *
+from pychord.interval import *
+from pychord.mode import *
+from pychord.note import *
+from pychord.ratio import *
+from pychord.tone import *
+
+__all__ = [
+    # Classes
+    "Tone",
+    "Ratio",
+    "Interval",
+    "Note",
+    "Chord",
+    "Mode",
+    "Scale",
+    # Constants
+    "OCTAVE_RATIO",
+    "SEMITONE_RATIO",
+    "C0_FREQUENCY",
+    "SEMITONE",
+    "MINOR_SECOND",
+    "WHOLETONE",
+    "MAJOR_SECOND",
+    "MINOR_THIRD",
+    "MAJOR_THIRD",
+    "PERFECT_FOURTH",
+    "AUGMENTED_FOURTH",
+    "TRITONE",
+    "DIMINISHED_FIFTH",
+    "PERFECT_FIFTH",
+    "MINOR_SIXTH",
+    "MAJOR_SIXTH",
+    "MINOR_SEVENTH",
+    "MAJOR_SEVENTH",
+    "OCTAVE",
+    "IONIAN",
+    "PHRYGIAN",
+    "DORIAN",
+    "LYDIAN",
+    "MIXOLYDIAN",
+    "AEOLIAN",
+    "LOCRIAN",
+]
